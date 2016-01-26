f30b29e5abf6b4b535d63b3aa807acf6d6c79d03
Genève Campbell <genevec@users.noreply.github.com>
2016-01-26 13:57:56 -0500

    Update amber.php

--- amber/amber.php
+++ amber/amber.php
@@ -7,7 +7,7 @@
  * Author: Berkman Center for Internet & Society
  * Author URI: https://cyber.law.harvard.edu
  * License: GPL3
- * Text Domain: lang-amberlink
+ * Text Domain: amberlink-lang
  */
 
 define("AMBER_ACTION_NONE",0);
